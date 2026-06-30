5317f75d9fd9ea372895ea4bb3f2020aa3a5aebf
Joao Silva <jotal96@gmail.com>
2026-06-30 10:39:57 +0100

    apresentar os itens da API com base nos cartoes do bootstrap. TODO: colocar o card dentro de um componente para ser re-utilizável

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,41 @@
 'use client';
 
+import {getItensMenu, IMG_URL} from "@/api/api";
+import {useEffect, useState} from "react";
 
-
-
-
-import {getItensMenu} from "@/api/api";
-import {useEffect} from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export default function Home() {
+   const [dadosMenu, setDadosMenu] = useState([]);
+
    useEffect(async () => {
       let dados = await getItensMenu();
-      console.log(dados);
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setDadosMenu(dados);
    }, []);
 
-   return <></>;
+   const htmlMenu = dadosMenu.map((item) => {
+      return(
+          <Card key={item.id} style={{ width: '15rem', margin: '1rem', padding: '1rem',
+              display: 'flex', justifyContent: 'space-between'}}>
+             <div style={{display: 'flex', justifyContent: 'center'}}>
+               <Card.Img style={{ width: '80px' }} src={IMG_URL+item.Foto.formats.thumbnail.url} />
+             </div>
+            <Card.Body>
+               <Card.Title>{item.Titulo}</Card.Title>
+               <Card.Text>
+                  {item.Descricao}
+               </Card.Text>
+               <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>)
+   });
+
+   return <>
+      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}>
+         {htmlMenu}
+      </div>
+
+   </>;
 }
